df1d91c87f50672ae55cfbd2a949c1f67195a0e2
Jing Hong Gang <honggangjing@gmail.com>
2021-11-04 09:28:40 +0800

    feat: 创建 stream demo

--- stream-adventure/demo/high-water-mark/writable.js
+++ stream-adventure/demo/high-water-mark/writable.js
@@ -1,3 +1,40 @@
 /**
  * Created by JingHongGang on 2021/11/3.
  */
+var Stream = require('stream')
+
+var source = ['a', 'b', 'c', 'd', 'e', 'f']
+
+var readable = Stream.Readable({
+  highWaterMark: 2,
+  read: function () {
+    var data = source.shift() || null
+    this.push(data)
+  },
+})
+
+const writable = Stream.Writable({
+  highWaterMark: 2,
+  write: function (data, _, next) {
+    console.log('write', data)
+    setTimeout(() => {
+      console.log('write next')
+      next()
+    }, 1000)
+  }
+})
+
+readable.on('data', function (data) {
+  console.log('readable.data', data)
+  if (!writable.write(data)) {
+    console.log('readable.pause')
+    readable.pause()
+  }
+})
+
+writable.on('drain', function () {
+  console.log('readable.resume')
+  readable.resume()
+})
+
+
---
+++ stream-adventure/demo/transform/transform.js
@@ -0,0 +1,43 @@
+/**
+ * Created by JingHongGang on 2021/11/3.
+ */
+var Stream = require('stream')
+
+var transform = createTransform()
+
+transform.pipe(process.stdout)
+
+transform.on('finish', function () {
+  console.log('\nfinish')
+})
+
+transform.on('end', function () {
+  console.log('\nend')
+})
+
+transform.write('a')
+transform.write('b')
+transform.end('c')
+
+function createTransform() {
+  var input = []
+  return Stream.Transform({
+    objectMode: true,
+    transform: function (buf, _, next) {
+      console.log('transform', buf.toString())
+      input.push(buf)
+      next()
+    },
+    flush: function (next) {
+      console.log('flush')
+      var buf
+      while (buf = input.pop()) {
+        this.push(buf)
+      }
+      setTimeout(() => {
+        this.push('extra')
+        next()
+      }, 10)
+    },
+  })
+}
